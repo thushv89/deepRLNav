a33080322c00f9832a7478147c05ea4cca17e636
thushv89 <thushv@gmail.com>
2016-08-15 16:11:37 +1000

    one setting in utils.py to change between simulation and wombot

--- python/utils.py
+++ python/utils.py
@@ -1,9 +1,16 @@
 
+
+############# THE MOST IMPORTANT SETTING ################
+
+TYPE = 'SIM' #SIM or REAL
+
+#########################################################
+
 #COMMON TOPICS
-CAMERA_IMAGE_TOPIC = "/camera/image" # sim /camera/image real /camera/image_color
+CAMERA_IMAGE_TOPIC = "/camera/image_color" if TYPE == 'REAL' else "/camera/image/" # sim /camera/image real /camera/image_color
 LASER_SCAN_TOPIC = "/scan" # sim and real /scan
 ODOM_TOPIC = "/odom"
-CMD_VEL_TOPIC = "/cmd_vel" #sim /cmd_vel real /wombot/cmd_vel
+CMD_VEL_TOPIC = "/wombot/cmd_vel" if TYPE == 'REAL' else "/cmd_vel" #sim /cmd_vel real /wombot/cmd_vel
 
 #MY TOPICS
 ACTION_STATUS_TOPIC = "/action_status"
@@ -16,25 +23,25 @@ INITIAL_RUN_TOPIC = "/initial_run"
 GOAL_TOPIC = "/move_base_simple/goal"
 
 # CAMERA
-CAMERA_FREQUENCY = 10
+CAMERA_FREQUENCY = 7 if TYPE=='REAL' else 10
 IMG_W, IMG_H = 640, 480
 THUMBNAIL_W, THUMBNAIL_H = 128, 96
 IMG_SAVE_SKIP = 5 # how many images skipped when saving sequence
 
 #BUMP DETECTION
-BUMP_1_THRESH = 0.6 #sim 0.6 real 0.1
-BUMP_02_THRESH = 0.7 #sim 0.7 real 0.15
+BUMP_1_THRESH = 0.2 if TYPE=='REAL' else 0.6 #sim 0.6 real 0.2
+BUMP_02_THRESH = 0.25 if TYPE=='REAL' else 0.7 #sim 0.7 real 0.25
 NO_RETURN_THRESH = 0.01
-REVERSE_PUBLISH_DELAY = 0.12 # real 0.12  
+REVERSE_PUBLISH_DELAY = 0.15 # real 0.15
 ZERO_VEL_PUBLISH_DELAY = 0.05 # publish 0 valued cmd_vel data
 
 #LASER
-LASER_FREQUENCY = 10
-LASER_POINT_COUNT = 180
-LASER_ANGLE = 180
+LASER_FREQUENCY = 40 if TYPE=='REAL' else 10 #sim 10 real 40 
+LASER_POINT_COUNT = 1080 if TYPE=='REAL' else 180
+LASER_ANGLE = 270 if TYPE=='REAL' else 180 # 
 
-PREF_FREQUENCY = 5
+PREF_FREQUENCY = 7 if TYPE=='REAL' else 10
 
 #COMMANDS
 GOAL_CANCEL_CMD = "rosservice call /autonomy/path_follower/cancel_request"
-MOVE_ROBOT_CMD = "./atrv_move_robot " # sim ./atrv_move_robot real ./wombot_move_robot
+MOVE_ROBOT_CMD = "./wombot_move_robot " if TYPE=='REAL' else "./atrv_move_robot "# sim ./atrv_move_robot real ./wombot_move_robot
