def2f54b83bd12b3cdfc381f1b07dc2e284454c9
thushv89 <thushv@gmail.com>
2016-10-22 15:36:05 +1100

    video_encoder and pose_displayer updated

--- python/Encode_Video_OpenCV.py
+++ python/Encode_Video_OpenCV.py
@@ -7,10 +7,36 @@ import numpy as np
 import os
 import pickle
 
+combine_rviz = True
+data_type = 'office' #['office','outdoor']
 
-dir = "test_right_pose"+os.sep+"test_office_ra_new"
-width,height = 640,480
+
+img_w,img_h = 640,480
+rimg_w,rimg_h = 300,300
+res_img_w,res_img_h = 512,384
+res_rimg_w,res_rimg_h = 256,256
+
+if combine_rviz:
+    width,height = 768,480
+else:
+    width,height = 640,480
+
+#[(0,7),(7,20),(53,61),(84,108),(270,284),(375,385)]
+if data_type=='office':
+    rviz_dir = 'test_right_pose'+os.sep+'test_rviz_office_ra_new'
+    dir = "test_right_pose"+os.sep+"test_office_ra_new"
+    ranges = [(7,20),(85,108),(270,284),(378,385)] #for office
+    ranges_ep = [(0,50),(50,100),(250,300),(300,400)]
+    exclude = [dir + os.sep + 'img_8_0.png',dir + os.sep + 'img_378_90.png'] #office
+elif data_type=='outdoor':
+    dir = "test_right_pose/outdoor"+os.sep+"test_outdoor_ra_new"
+    rviz_dir = 'test_right_pose'+os.sep+'test_rviz_outdoor_ra_new'
+    ranges = [(0,11),(192,206),(289,304)]
+    ranges_ep = [(0,50),(150,200),(250,300)] # outdoor
+    exclude = []
 pose_type = 'pkl'
+
+img_ep_ids = []
 
 episode_dict = {}
 if pose_type=='pkl':
@@ -22,7 +48,8 @@ if pose_type=='pkl':
             max_pkl_idx=pkl_idx
 
     for i in range(max_pkl_idx):
-
+        if data_type=='outdoor' and i==372:
+            continue
         with open(dir+os.sep+'pose_'+str(i)+'.pkl', 'rb') as f:
             data = pickle.load(f)
             for point in data:
@@ -47,6 +74,8 @@ elif pose_type=='log':
 #correct 0 episode entries for each restart
 zero_ep_entries = []
 for k in range(np.max(episode_dict.keys())):
+    if data_type=='outdoor' and k==372:
+        continue
     if k!=0:
         if episode_dict[k]==0:
             episode_dict[k]=episode_dict[k-1]+1
@@ -77,7 +106,6 @@ file_name_dict = {}
 
 for name in img_names:
     id_str,val_str = name[4:-4].split('_')
-    print id_str,val_str
     id_int,val_int = int(id_str),int(val_str)
     if id_int in file_name_dict:
         file_name_dict[id_int].append(val_int)
@@ -86,6 +114,8 @@ for name in img_names:
 
 img_idx_projected_data = {}
 for i in range(np.max(file_name_dict.keys())):
+    if data_type=='outdoor' and i==372:
+        continue
     if i==0:
         img_idx_projected_data[i]=(episode_dict[i],-1,None)
     else:
@@ -94,19 +124,66 @@ for i in range(np.max(file_name_dict.keys())):
         else:
             img_idx_projected_data[i]=(episode_dict[i],action_dict[episode_dict[i]],prob_dict[episode_dict[i]])
 
-fourcc = cv2.cv.CV_FOURCC(*'XVID')
-video = cv2.VideoWriter('new-video.avi',fourcc,3,(width,height),1)
+fourcc = cv2.cv.CV_FOURCC('X','V','I','D')
+video = cv2.VideoWriter('video-'+data_type+'-ra.avi',fourcc,3,(width,height),1)
 
 rect_h = 100
 rect_w = 40
 rect_x0 = 350
 padding_w = 25
 
+def compose_combined_frame(fname,rname,f_id):
+    global img, p
+    img = cv2.imread(fname)
+    img_resized = cv2.resize(img,(res_img_w,res_img_h))
+    extended_img = np.append(np.zeros((img_h-res_img_h,res_img_w,3),dtype='uint8'),img_resized,axis=0)
+
+    img2 = cv2.imread(rname)
+    if img2 is not None:
+        img2_resized = cv2.resize(img2,(res_rimg_w,res_rimg_h))
+        img2_extended = np.append(np.zeros((img_h-res_rimg_h,res_rimg_w,3),dtype='uint8'),img2_resized,axis=0)
+    else:
+        img2_extended = np.zeros((img_h,res_rimg_w,3),dtype='uint8')
+
+    comb_img = np.asarray(np.concatenate((extended_img, img2_extended), axis=1),dtype='uint8')
+
+    #title
+    if data_type=='office':
+        cv2.putText(comb_img,'Office - RA-DAE - 400 Episodes',(25,50),cv2.FONT_HERSHEY_PLAIN, 1.5,(255,255,255),2)
+    elif data_type=='outdoor':
+        cv2.putText(comb_img,'Outdoor - RA-DAE - 400 Episodes',(25,50),cv2.FONT_HERSHEY_PLAIN, 1.5,(255,255,255),2)
+    if not fname_display is None:
+        cv2.putText(comb_img, fname_display, (50, 50), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
+
+    cv2.putText(comb_img, episode_display, (520, 120), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
+    cv2.putText(comb_img, action_display, (520, 160), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
+
+    if img_idx_projected_data[f_id][1]!=-1:
+        action_prob_display = 'P('+action_str+'): %.2f'%(img_idx_projected_data[f_id][2][img_idx_projected_data[i][1]])
+        cv2.putText(comb_img, action_prob_display, (520, 200), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
+
+    if img_idx_projected_data[f_id][2] is not None:
+        probs_display = 'Probs:%.2f,%.2f,%.2f' % (
+        img_idx_projected_data[f_id][2][0], img_idx_projected_data[f_id][2][1], img_idx_projected_data[f_id][2][2])
+        cv2.putText(comb_img, probs_display, (200, 460), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
+
+        rect_x0_prob = 280
+        for p in img_idx_projected_data[i][2]:
+            rect_y1_prob = int(rect_h * p)
+            if p < 0.45:
+                color = (0, 0, 255)
+            else:
+                color = (0, 255, 0)
+            cv2.rectangle(comb_img, (rect_x0_prob, 420), (rect_x0_prob + rect_w, 420 - rect_y1_prob), color, -1)
+            rect_x0_prob = rect_x0_prob + rect_w + padding_w
+    return comb_img
+
 def compose_frame(fname):
     global img, p
     img = cv2.imread(fname)
 
-    cv2.putText(img, fname_display, (50, 25), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
+    if not fname_display is None:
+        cv2.putText(img, fname_display, (50, 25), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
     cv2.putText(img, episode_display, (50, 50), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
     cv2.putText(img, action_display, (50, 75), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
     if img_idx_projected_data[i][2] is not None:
@@ -126,35 +203,61 @@ def compose_frame(fname):
 
     return img
 
-for i in range(np.max(file_name_dict.keys())//10):
+
+for i in range(np.max(file_name_dict.keys())):
+    if data_type=='outdoor' and i==372:
+        continue
+    if ranges is not None:
+        in_range = [True if r[0]<=img_idx_projected_data[i][0]<=r[1] else False for r in ranges]
+        if not np.any(in_range):
+            continue
+        else:
+            r_id = np.where(np.asarray(in_range)==True)[0]
+            img_ep_ids.append(i)
+
     while len(file_name_dict[i])>0:
 
         fname = dir+os.sep+'img_'+str(i)+'_'+str(np.min(file_name_dict[i]))+'.png'
+        if combine_rviz:
+            rname = rviz_dir+os.sep+'rviz_'+str(i)+'_'+str(np.min(file_name_dict[i]))+'.png'
+        if fname not in exclude:
 
-        print "Processing frame:%s "%(fname)
-        fname_display = 'image-'+str(i)+'-'+str(np.min(file_name_dict[i]))
-        episode_display = 'Episode: '+str(img_idx_projected_data[i][0])
-        if img_idx_projected_data[i][1]==0:
-            action_str = 'Left'
-        elif img_idx_projected_data[i][1]==1:
-            action_str = 'Straight'
-        elif img_idx_projected_data[i][1]==2:
-            action_str = 'Right'
-        elif img_idx_projected_data[i][1]==-1:
-            action_str = 'Initial Step'
+            print "Processing frame:%s "%(fname)
 
-        action_display = 'Action:'+action_str
+            if ranges is not None:
+                fname_display = None
+                episode_display = 'Episode: '+str(ranges_ep[r_id][0])+'-'+str(ranges_ep[r_id][1])
+            else:
+                fname_display = 'image-'+str(i)+'-'+str(np.min(file_name_dict[i]))
+                episode_display = 'Episode: '+str(img_idx_projected_data[i][0])
+            if img_idx_projected_data[i][1]==0:
+                action_str = 'Left'
+            elif img_idx_projected_data[i][1]==1:
+                action_str = 'Straight'
+            elif img_idx_projected_data[i][1]==2:
+                action_str = 'Right'
+            elif img_idx_projected_data[i][1]==-1:
+                action_str = 'Initial Step'
 
-        img = compose_frame(fname)
-        video.write(img)
-        if len(file_name_dict[i])==1:
-            for _ in range(3):
-                img = compose_frame(fname)#print "Writing Video ..."
-                video.write(img)
+            action_display = 'Action:'+action_str
+
+            if combine_rviz:
+                img = compose_combined_frame(fname,rname,i)
+            else:
+                img = compose_frame(fname)
+            video.write(img)
+            if len(file_name_dict[i])==1:
+                for _ in range(1):
+                    if combine_rviz:
+                        img = compose_combined_frame(fname,rname,i)#print "Writing Video ..."
+                    else:
+                        img = compose_frame(fname)
+                    video.write(img)
 
         file_name_dict[i].remove(np.min(file_name_dict[i]))
 
 print "Releasing Video ..."
+print img_ep_ids
 video.release()
 cv2.destroyAllWindows()
 
--- python/pose_broadcaster.py
+++ python/pose_broadcaster.py
@@ -14,9 +14,18 @@ import os
 import pickle
 import time
 import scipy.misc as sm
+from multiprocessing import Pool
 
-dir = 'test_right_pose'+os.sep+'test_office_ra_new'
-rviz_dir = 'test_right_pose'+os.sep+'test_rviz_office_ra_new'
+data_type = 'outdoor'
+
+if data_type=='office':
+    dir = 'test_right_pose'+os.sep+'test_office_ra_new'
+    rviz_dir = 'test_right_pose'+os.sep+'test_rviz_office_ra_new'
+    img_eps = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 374, 375, 376, 377, 378, 379, 380, 381]
+elif data_type =='outdoor':
+    dir = 'test_right_pose/outdoor'+os.sep+'test_outdoor_ra_new'
+    rviz_dir = 'test_right_pose'+os.sep+'test_rviz_outdoor_ra_new'
+    img_eps = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305]
 data_type ='pkl' #pkl or log
 pose_arr = []
 unproc_image_data = []
@@ -25,12 +34,15 @@ img_seq_id = 0
 episodes = []
 img_seq_indices = []
 published_pose = False
+ep_to_algo_ep_map = {}
+prev_e = 0
+
+
 def callback_img(msg):
     global published_pose,ep_id,img_seq_id
-    data = msg.data
-    num_data=[]
     if published_pose:
-        #unproc_image_data.append((ep_id,img_seq_id,msg.data))
+        #print 'Img added ',ep_id,img_seq_id
+        unproc_image_data.append((ep_id,img_seq_id,msg.data))
         published_pose = False
 
 def save_image(img_data_with_ep_seq):
@@ -42,12 +54,11 @@ def save_image(img_data_with_ep_seq):
     img_np_2=np.empty((300,300,3),dtype=np.int16)
     for i in range(0,len(img_data),4):
         r_idx,c_idx=(i//4)//300,(i//4)%300
-        img_np_2[r_idx,c_idx,(i%4)]=int(ord(img_data[i]))
+        img_np_2[r_idx,c_idx,(i%4)]=int(ord(img_data[i+2]))
         img_np_2[r_idx,c_idx,(i%4)+1]=int(ord(img_data[i+1]))
-        img_np_2[r_idx,c_idx,(i%4)+2]=int(ord(img_data[i+2]))
+        img_np_2[r_idx,c_idx,(i%4)+2]=int(ord(img_data[i]))
 
     sm.imsave(rviz_dir + os.sep + 'rviz_' +str(ep) + '_' + str(seq) + ".png",img_np_2)
-
 
 if __name__=='__main__':
     global ep_id,img_seq_id
@@ -68,9 +79,19 @@ if __name__=='__main__':
             with open(dir+os.sep+'pose_'+str(i)+'.pkl', 'rb') as f:
                 data = pickle.load(f)
                 for point in data:
+                    px,py,pz,ox,oy,oz,ow = point[3][0],point[3][1],point[3][2],point[3][3],point[3][4],point[3][5],point[3][6]
+                    if point[0]==11:
+                        px+=0.1
+                    if point[0]==12:
+                        px+=0.2
+                    if point[0]==13 or point[0]==14:
+                        px+=0.3
+                    if point[0]==376 or point[0]==270:
+                        px-=0.1
                     episodes.append(point[0])
-                    img_seq_indices.append(point[2])
-                    pose_arr.append(point[3])
+                    img_seq_indices.append(point[1])
+                    pose_arr.append([px,py,pz,ox,oy,oz,ow])
+                    ep_to_algo_ep_map[point[0]]=point[2]
 
         print(len(pose_arr))
     elif data_type=='log':
@@ -91,9 +112,9 @@ if __name__=='__main__':
     marker.id = 0
 
     marker.action = marker.ADD
-    marker.scale.x = 0.02
-    marker.scale.y = 0.02
-    marker.scale.z = 0.02
+    marker.scale.x = 0.05
+    marker.scale.y = 0.05
+    marker.scale.z = 0.05
     marker.color.a = 1.0
     marker.color.r = 1.0
     marker.color.g = 0.0
@@ -106,25 +127,32 @@ if __name__=='__main__':
 
     arrow_marker.type = marker.ARROW
     arrow_marker.action = marker.ADD
-    arrow_marker.scale.x = 0.1
-    arrow_marker.scale.y = 0.1
-    arrow_marker.scale.z = 0.1
+    arrow_marker.scale.x = 0.25
+    arrow_marker.scale.y = 0.15
+    arrow_marker.scale.z = 0.01
     arrow_marker.color.a = 1.0
     arrow_marker.color.r = 1.0
     arrow_marker.color.g = 1.0
     arrow_marker.color.b = 0.0
 
     for p_i,(e,ims,pose) in enumerate(zip(episodes,img_seq_indices,pose_arr)):
-        if p_i==len(pose_arr):
-            break
+
+        if e not in img_eps:
+            continue
+
         while published_pose:
             True
+
+        if np.abs(prev_e-e)>10:
+                marker.points = []
+                print "blank"
+                rospy.sleep(1)
 
         if marker_type is 'sphere':
             marker = Marker()
 
             marker.header.frame_id = "map"
-            marker.id = (p_i)%250
+            marker.id = p_i
 
             marker.action = marker.ADD
             marker.scale.x = 0.02
@@ -169,15 +197,26 @@ if __name__=='__main__':
 
         ep_id,img_seq_id = e,ims
 
+        if len(unproc_image_data)>500:
+            pool = Pool(50)
+            pool.map(save_image, unproc_image_data)
+            pool.close()
+            pool.join()
+            unproc_image_data = []
+
         pose_pub.publish(marker)
         pose_arrow_pub.publish(arrow_marker)
+        print 'pose added ',ep_id,img_seq_id
+
         published_pose = True
         #br.sendTransform((pose[0],pose[1],pose[2]),(pose[3],pose[4],pose[5],pose[6]),rospy.Time.now(),"map","/robot_pose")
 
-        prev_x,prev_y,prev_z = pose[0],pose[1],pose[2]
-        rospy.sleep(0.05)
+        prev_e = e
+        rospy.sleep(0.1)
 
-    #for img in unproc_image_data:
-        #save_image(img)
+
+
+    for img in unproc_image_data:
+        save_image(img)
     rospy.spin()
 
